f7077882decc4d4b8c019c97e713e4254dfd237d
Subhash Guttikonda <subgutti@gmail.com>
2015-09-14 21:31:28 -0700

    Format code as per PEP8 guidelines

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,8 +1,9 @@
 import fresh_tomatoes
 import media
 
+
 class EntertainmentCenter():
-    
+
     """ Creates a list of favorite Movies and displays them on a webpage."""
 
     def create_movies(self):
@@ -10,12 +11,36 @@ class EntertainmentCenter():
 
             Returns : list of favorite movies
         """
-        toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life", "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg", "https://www.youtube.com/watch?v=ZZv1vki4ou4")
-        avatar = media.Movie("Avatar", "A marine in alien planet","https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg","https://www.youtube.com/watch?v=5PSNL1qE6VY")
-        departed = media.Movie("Departed", "Cops and Gangsters", "https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg", "https://www.youtube.com/watch?v=auYbpnEwBBg")
-        shrek = media.Movie("Shrek", "Fairy tale", "https://upload.wikimedia.org/wikipedia/en/3/39/Shrek.jpg", "https://www.youtube.com/watch?v=W37DlG1i61s")
-        cars = media.Movie("Cars", "Cars animation movie", "https://upload.wikimedia.org/wikipedia/en/3/34/Cars_2006.jpg" ,"https://www.youtube.com/watch?v=SbXIj2T-_uk")   
-        inside_out = media.Movie("Inside out", "animation", "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg", "https://www.youtube.com/watch?v=seMwpP0yeu4")
+        toy_story = media.Movie(
+            "Toy Story",
+            "A story of a boy and his toys that come to life",
+            "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+            "https://www.youtube.com/watch?v=ZZv1vki4ou4")
+        avatar = media.Movie(
+            "Avatar",
+            "A marine in alien planet",
+            "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",  # noqa
+            "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+        departed = media.Movie(
+            "Departed",
+            "Cops and Gangsters",
+            "https://upload.wikimedia.org/wikipedia/en/5/50/Departed234.jpg",
+            "https://www.youtube.com/watch?v=auYbpnEwBBg")
+        shrek = media.Movie(
+            "Shrek",
+            "Fairy tale",
+            "https://upload.wikimedia.org/wikipedia/en/3/39/Shrek.jpg",
+            "https://www.youtube.com/watch?v=W37DlG1i61s")
+        cars = media.Movie(
+            "Cars",
+            "Cars animation movie",
+            "https://upload.wikimedia.org/wikipedia/en/3/34/Cars_2006.jpg",
+            "https://www.youtube.com/watch?v=SbXIj2T-_uk")
+        inside_out = media.Movie(
+            "Inside out",
+            "animation",
+            "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_%282015_film%29_poster.jpg",  # noqa
+            "https://www.youtube.com/watch?v=seMwpP0yeu4")
 
         movies_list = [toy_story, avatar, departed, shrek, cars, inside_out]
         return movies_list
--- media.py
+++ media.py
@@ -1,15 +1,21 @@
 import webbrowser
 
+
 class Movie():
-    
+
     """ Data structure to store movie related information.
         1. Title
         2. Story line
         3. Poster url
         4. Trailer url
     """
-    
-    def __init__(self, movie_title, movie_storyline, movie_poster_url, movie_youtube_url):
+
+    def __init__(
+            self,
+            movie_title,
+            movie_storyline,
+            movie_poster_url,
+            movie_youtube_url):
         print("Initialize the movie object for " + movie_title)
         self.title = movie_title
         self.storyline = movie_storyline
